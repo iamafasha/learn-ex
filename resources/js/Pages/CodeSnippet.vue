c609da686f1d314e3a196cc0c69d244a5d161882
iamafasha <afashaisakiye@gmail.com>
2022-12-09 13:30:51 +0300

    Add title bar

--- resources/js/Pages/CodeSnippet.vue
+++ resources/js/Pages/CodeSnippet.vue
@@ -1,12 +1,16 @@
 <template>
-    
 
+
+    <Head>
+    <title>Code Snippet</title>
+    <meta name="description" content="Your page description">
+    </Head>
     <Editor value="console.log('HelloWorld')"></Editor>
 </template>
 
 
 <script setup>
-
+import { Head } from '@inertiajs/inertia-vue3'
 
 import Editor from '../components/Editor/Index.vue';
 
