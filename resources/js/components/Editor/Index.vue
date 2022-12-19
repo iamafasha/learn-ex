adb18e89408544b9c99b80f513775a12c3605696
iamafasha <afashaisakiye@gmail.com>
2022-12-19 19:55:55 +0300

    Migrate Editor component to composition and add marker

--- resources/js/components/Editor/Index.vue
+++ resources/js/components/Editor/Index.vue
@@ -2,13 +2,13 @@
     <div class="md:flex w-full h-full justify-center items-center code-editor">
         <div class=" md:w-2/3  h-full">
             <div class="ace-container h-full">
-                <div class="ace-editor relative h-full" ref="ace"></div>
+                <div class="ace-editor relative h-full" ref="ace_text_area"></div>
             </div>
         </div>
     </div>
   </template>
 
-  <script>
+  <script setup>
     import ace from 'ace-builds'
     import 'ace-builds/src-noconflict/snippets/javascript'
     import 'ace-builds/src-noconflict/snippets/html'
@@ -21,76 +21,170 @@
     import 'ace-builds/src-noconflict/ext-language_tools'
     import 'ace-builds/src-noconflict/theme-monokai'
     import {Mode as JavaScriptMode} from 'ace-builds/src-min-noconflict/mode-javascript'
+    import { reactive, ref, onMounted } from 'vue'
 
 
-    export default {
-        props: {
-            value: String
-        },
-        mounted() {
-            this.aceEditor = ace.edit(this.$refs.ace, {
-                value: this.value ? this.value : '',
+    const ace_text_area = ref(null)
+
+    const props = defineProps({ value: String })
+
+    const state = reactive({
+        aceEditor:null,
+        value:""
+    })
+
+    const change = () => {
+                $emit('input', state.aceEditor.getSession().getValue())
+    }
+
+    onMounted(()=>{
+        state.value = props.value
+        state.aceEditor = ace.edit(ace_text_area.value, {
+                value: state.value ? state.value : '',
                 theme: 'ace/theme/twilight',
                 tabSize: 4
             });
 
-            this.aceEditor.setOptions({
-                enableSnippets: true,
-                enableLiveAutocompletion: true,
-                enableBasicAutocompletion: true
-            });
+    var label = "I am red remote cursor :)";
+    var marker_element = document.createElement("div");
+    var cursor_element = document.createElement("div");
+    cursor_element.className = "ace-multi-cursor";
+    cursor_element.style.background = "red";
+    marker_element.append(cursor_element);
 
-            // const marker = this.aceEditor.getSession().addDynamicMarker({
-            //     id: "marker-layer",
-            //     update: function(html, markerLayer, session, config) {
-            //         return html;
-            //     }
-            // },true)
+    var tooltip_element = document.createElement("div");
+    tooltip_element.className = "ace-multi-cursor-tooltip";
+    tooltip_element.style.background = "red";
+    tooltip_element.style.opacity = "1";
+    tooltip_element.innerHTML = label;
+    marker_element.append(tooltip_element);
 
-            // this.aceEditor.getSession().addMarker(new ace.Range(0, 0, 0, 0),'mark-layer', "fullLine", true)
-            // console.log(this.aceEditor.getSession().addDynamicMarker);
+    state.aceEditor.session.addDynamicMarker({
+        contentNode: null,
+        update: function(_, layer, session, config) {
 
-            this.aceEditor.session.setMode(new JavaScriptMode())
-            this.aceEditor.getSession().on('change', this.change)
-        },
-        data() {
-            return {
-                aceEditor: null,
+
+            var top = layer.$getTop(5, config);
+            var left = layer.$padding + 6 * config.characterWidth;
+            var height = config.lineHeight;
+
+            var cursor_top = top + 2;
+            var cursor_height = height - 3;
+            var cursor_left = left;
+            var cursor_width = 2;
+
+            // console.log(cursor_element)
+            // cursor_element.style.position = "relative";
+            cursor_element.style.height = `${cursor_height}px`;
+            cursor_element.style.width = `${cursor_width}px`;
+            cursor_element.style.top = `${cursor_top}px`;
+            cursor_element.style.left = `${cursor_left}px`;
+
+            let toolTipTop = cursor_top - height;
+            if (toolTipTop < 5) {
+            toolTipTop = cursor_top + height - 1;
             }
-        },
-        methods: {
-            change() {
-                this.$emit('input', this.aceEditor.getSession().getValue())
-            }
+
+            const toolTipLeft = cursor_left;
+            tooltip_element.style.top = `${toolTipTop - 2}px`;
+            tooltip_element.style.left = `${toolTipLeft - 2}px`;
+
+            // marker_element.remove();
+            layer.elt("remote-cursor", "");
+            const parentNode = layer.element.childNodes[layer.i - 1] || layer.element.lastChild;
+            parentNode.appendChild(marker_element);
         }
-    }
+    },true)
+
+    state.aceEditor.session.setMode(new JavaScriptMode())
+    state.aceEditor.getSession().on('change',() => change )
+    })
+
   </script>
 
   <style lang='scss' scoped>
+ .ace-editor {
+	/*height: 150px;*/
+	position: absolute;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
+}
 
-// .marker-layer {
-//         animation: blink 1s infinite;
-//         background-color: red;
-//         border: 1px solid red;
-//     }
+.other_user_selection {
+	position: absolute;
+	z-index: 6;
+}
 
-//     @keyframes blink {
-//     0% {
-//         opacity: 1;
-//     }
-//     50% {
-//         opacity: 0;
-//     }
-//     100% {
-//         opacity: 1;
-//     }
-//     }
-  .code-editor{
-    position: relative;
-    height: 90vh;
-  }
-    .ace-container {
-      position: relative;
-      width: 100%;
-    }
+.other_user_selection.blue {
+	background-color: blue;
+	background-color: rgba(0, 0, 255, 0.5);
+}
+
+/*ace Collab ext*/
+.ace-multi-cursor {
+  position: absolute;
+  pointer-events: auto;
+  z-index: 10;
+}
+
+.ace-multi-cursor-tooltip {
+  position: absolute;
+  white-space: nowrap;
+  color: #FFFFFF;
+  text-shadow: 0 0 1px #000000;
+  opacity: 1;
+  font-size: 12px;
+  padding: 2px;
+  font-family: sans-serif;
+
+  transition: opacity 0.5s ease-out;
+  -webkit-transition: opacity 0.5s ease-out;
+  -moz-transition: opacity 0.5s ease-out;
+  -ms-transition: opacity 0.5s ease-out;
+  -o-transition: opacity 0.5s ease-out;
+}
+
+.ace-multi-selection {
+  position: absolute;
+  pointer-events: auto;
+  z-index: 10;
+  opacity: 0.3;
+}
+
+.ace-radar-view {
+  position: relative;
+  min-width: 6px;
+}
+
+.ace-radar-view-scroll-indicator {
+  position: absolute;
+  left: 0;
+  right: 0;
+  border-radius: 4px;
+  cursor: pointer;
+  border-style: double;
+  border-width: 3px;
+}
+
+.ace-radar-view-cursor-indicator {
+  position: absolute;
+  left: 0;
+  right: 0;
+  height: 4px;
+  border-radius: 3px;
+  cursor: pointer;
+  border: 1px solid black;
+}
+
+.ace-radar-view-wrapper {
+  position: relative;
+  float: left;
+
+  height: 100%;
+  width: 6px;
+
+  margin-right: 4px;
+}
   </style>
