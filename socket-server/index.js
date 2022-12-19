1e0bd501f8b3c0b6fd05cf04877a2e8803bf0b97
iamafasha <afashaisakiye@gmail.com>
2022-12-19 23:55:15 +0300

    Fix bug on wesocket server

--- socket-server/index.js
+++ socket-server/index.js
@@ -2,13 +2,11 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require('socket.io')(server, {
+const io= require('socket.io')(server, {
     cors: {
       origin: '*',
     }
   });
-
-const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
